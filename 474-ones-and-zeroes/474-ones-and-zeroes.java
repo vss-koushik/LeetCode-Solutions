8620f836e72e98ad1252439ce2e5167b1f2fd2a8
vss-koushik <76484655+vss-koushik@users.noreply.github.com>
2022-05-23 23:02:01 +0530

    Time: 47 ms (62.96%), Space: 43 MB (63.45%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,3 +1,27 @@
+// Space optimized Approach!
+class Solution {
+    public static int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for(String str : strs){
+            int one = 0;
+            int zero = 0;
+            for(char c : str.toCharArray()){
+                if(c == '1')
+                    one++;
+                else
+                    zero++;
+            }
+            for(int i = m; i >= zero; i--){
+                for(int j = n; j >= one; j--){
+                    if(one <= j && zero <= i)
+                        dp[i][j] = Math.max(dp[i][j],dp[i - zero][j - one] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+/* Iterative Solution 3D Array
 class Solution {
     Integer[][][] dp;
     public int largestSubset(String[] strs, int idx , int m , int n){
@@ -20,3 +44,5 @@ class Solution {
         return largestSubset(strs, 0 , m , n);    
     }
 }
+*/
+
