f9b6ddd8e01d2b5a16fb760e678a2d766c05082a
vss-koushik <76484655+vss-koushik@users.noreply.github.com>
2022-06-18 17:12:21 +0530

    Time: 1 ms (46.91%), Space: 40.7 MB (65.98%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,10 +2,22 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int[] row : dp)
-            Arrays.fill(row,-1);
-        return helper(0,0,m,n,dp);
+            Arrays.fill(row,0);
+        for(int i = m - 1; i>= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1) dp[i][j] = 1;
+                else {
+                if(i < m -1)
+                    dp[i][j] += dp[i + 1][j];
+                if(j < n - 1)
+                    dp[i][j] += dp[i][j + 1];
+               }
+            }
+        }
+        
+        return dp[0][0];
     }
-    
+    /* Memoization Solution
     private int helper(int i, int j, int m, int n, int[][] dp){
         if(i == m-1 && j == n-1){
             return 1;
@@ -20,5 +32,5 @@ class Solution {
             down = helper(i + 1, j, m, n,dp);
         dp[i][j] = right + down;
         return right + down; 
-    }
+    }*/
 }
